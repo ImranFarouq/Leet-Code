7419fe431132dbfec6e724a4f90f5c368957279d
Imran <74622153+ImranFarouq@users.noreply.github.com>
2023-10-14 21:34:23 +0530

    Update Recyclable and Low Fat Products.py

--- Select/Recyclable and Low Fat Products.py
+++ Select/Recyclable and Low Fat Products.py
@@ -23,7 +23,16 @@
 # | 3           |
 # +-------------+
 
+# SQL solution
+  
+SELECT product_id FROM products
+WHERE low_fats = 'Y' AND
+recyclable = 'Y';
+
+#  Pandas Solution
 import pandas as pd
 
 def find_products(products: pd.DataFrame) -> pd.DataFrame:
     return products[(products['low_fats']=='Y') & (products['recyclable']=='Y')][['product_id']]
+
+
